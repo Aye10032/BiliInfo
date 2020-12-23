0d0c41392960468ce76d7b17f57eb56e916e3fc2
Aye10032 <yeyu6_505@126.com>
2020-12-23 19:20:44 +0800

    update

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,10 +1,11 @@
 import com.aye10032.biliutil.UserInfo;
+import com.aye10032.biliutil.VideoInfo;
 
 public class test {
 
     public static void main(String[] args) {
         UserInfo userInfo = new UserInfo(40077740L);
-
+        VideoInfo videoInfo = new VideoInfo("BV1fa4y1H7Gz");
     }
 
 }
