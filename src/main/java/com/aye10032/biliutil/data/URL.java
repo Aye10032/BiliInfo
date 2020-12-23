44976b93f1e7545e8d3df22f1dd840088a6b0d93
Aye10032 <yeyu6_505@126.com>
2020-12-23 19:20:27 +0800

    存放视频地址

---
+++ src/main/java/com/aye10032/biliutil/data/URL.java
@@ -0,0 +1,8 @@
+package com.aye10032.biliutil.data;
+
+public class URL {
+
+    public static final String url_user_space = "http://api.bilibili.com/x/space/acc/info";
+
+    public static final String url_video_info = "http://api.bilibili.com/x/web-interface/view";
+}
