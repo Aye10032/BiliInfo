30dcb69c7d7e56368aae3df6975c41f23bb88a28
Aye10032 <yeyu6_505@126.com>
2021-01-05 10:27:24 +0800

    测试类

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,9 +1,11 @@
 import com.aye10032.biliutil.UserInfo;
 import com.aye10032.biliutil.VideoInfo;
+import org.junit.Test;
 
 public class test {
 
-    public static void main(String[] args) {
+    @Test
+    public void test(){
         UserInfo userInfo = new UserInfo(40077740L);
         VideoInfo videoInfo = new VideoInfo("BV1fa4y1H7Gz");
     }
