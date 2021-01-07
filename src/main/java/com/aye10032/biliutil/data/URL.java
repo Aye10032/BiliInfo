aa48b650174a3b343a338cac4f19d129455d18eb
Aye10032 <yeyu6_505@126.com>
2021-01-07 12:19:21 +0800

    改用HTTPS

--- src/main/java/com/aye10032/biliutil/data/URL.java
+++ src/main/java/com/aye10032/biliutil/data/URL.java
@@ -2,7 +2,7 @@ package com.aye10032.biliutil.data;
 
 public class URL {
 
-    public static final String url_user_space = "http://api.bilibili.com/x/space/acc/info";
+    public static final String url_user_space = "https://api.bilibili.com/x/space/acc/info";
 
-    public static final String url_video_info = "http://api.bilibili.com/x/web-interface/view";
+    public static final String url_video_info = "https://api.bilibili.com/x/web-interface/view";
 }
