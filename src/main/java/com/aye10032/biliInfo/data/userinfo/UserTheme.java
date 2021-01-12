d74b519f2a3c436f1e79257d29d8f7b387a5b3be
Aye10032 <yeyu6_505@126.com>
2021-01-12 18:49:04 +0800

    留空

---
+++ src/main/java/com/aye10032/biliInfo/data/userinfo/UserTheme.java
@@ -0,0 +1,10 @@
+package com.aye10032.biliInfo.data.userinfo;
+
+/**
+ * @program: biliinfo
+ * @description: 主题？存疑，目前无用
+ * @author: Aye10032
+ * @create: 2021-01-12 18:43
+ **/
+public class UserTheme {
+}
