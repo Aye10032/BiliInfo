9b47fbab5ca2c8d4e2bc81e8eb5da2cc884677f5
Aye10032 <yeyu6_505@126.com>
2021-01-13 23:02:03 +0800

    update version

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,7 @@
+import com.aye10032.biliInfo.LiveInfo;
 import com.aye10032.biliInfo.UserInfo;
 import com.aye10032.biliInfo.VideoInfo;
+import com.aye10032.biliInfo.data.InfoType;
 import org.junit.Test;
 
 import java.text.SimpleDateFormat;
@@ -10,6 +12,8 @@ public class test {
     public void test() {
         UserInfo userInfo = new UserInfo(40077740L);
         System.out.println(userInfo.getUserData().getUser_liveroom().getTitle());
+        LiveInfo liveInfo = new LiveInfo(1478953, InfoType.LIVE_ROOM_ID);
+        System.out.println(liveInfo.getLiveData().getLive_time_date());
 //        VideoInfo videoInfo = new VideoInfo("BV1fa4y1H7Gz");
 //
 //        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
